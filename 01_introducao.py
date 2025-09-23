b6c22d1cb8a4d5d67d0f8fe3dca3578e0441a2ff
Caio Comitre Rossi <49957185+caioross@users.noreply.github.com>
2025-09-23 17:27:18 -0300

    primeira aula
    primeira aula de python 1 na clarify

---
+++ 01_introducao.py
@@ -0,0 +1,10 @@
+﻿# Isso é um comentário ☺ ♥
+
+'''
+Isso é um comentario
+de várias linhas
+'''
+
+print('Ola Sao Paulo')
+print("Ola Belem do Para")
+print("Ola 'Fortaleza' Ceara")
---
+++ 02_variaveis.py
@@ -0,0 +1,24 @@
+""" Trabalhando com tipos e Variaveis """
+
+nome = "Caio" # string
+sobrenome = 'Comitre Rossi'
+outronome = nome
+idade = 38 # valor inteiro
+altura = 1.81
+bermuda = False # boolean
+calca = True
+
+# exibir o conteudo da variavel
+print(nome)
+print(nome + " " + sobrenome + " tem " + str(idade) + " anos.") 
+
+print(idade + 2)
+
+textoVariasLinhas = '''
+uma linha
+outra linha
+mais uma
+'''
+print(textoVariasLinhas)
+
+
